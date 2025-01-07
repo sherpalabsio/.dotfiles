835e885497103f16f23e2d66dde90e3bab6d28e6
Peter Toth <tothpeter08@gmail.com>
2025-01-08 00:54:14 +0700

    Elixir: Enable saving the IEx command history

--- elixir/main.sh
+++ elixir/main.sh
@@ -1,2 +1,5 @@
+export ERL_HISTFILE="$DROPBOX_PATH/work/system/.erl_history"
+export ERL_AFLAGS="-kernel shell_history enabled shell_history_path '\"$ERL_HISTFILE\"'"
+
 # Phoenix console like Rails console
 alias pc='iex -S mix phx.server'
