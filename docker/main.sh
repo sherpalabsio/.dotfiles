1e904568a3feabdde788f130c1f3d0e2e5ffb188
Peter Toth <tothpeter08@gmail.com>
2025-01-12 22:54:08 +0700

    Add Docker shortcut to stop containers started by other projects

--- docker/main.sh
+++ docker/main.sh
@@ -9,23 +9,45 @@ alias de='docker_compose_up && docker exec -it ${DOCKER_CONTAINER_NAME:-$(basena
 alias con='de /bin/bash'
 
 alias up="docker_start_daemon && docker-compose up -d"
+# Up build - Build the container(s) and start them
 alias upb="docker_start_daemon && docker-compose up --build -d"
+# Down - Stop all running containers started with docker-compose from the current directory
 alias down="docker-compose down"
 # Down all - Stop all running containers
 alias downa='docker stop $(docker ps -q)'
+
+# Stop containers started by other projects
+# Useful when two projects are using PostgreSQL with the same port
+down_others() {
+  # In order to distinguish between the containers of different projects,
+  # the COMPOSE_PROJECT_NAME env variable must be set.
+  if [ -z "$COMPOSE_PROJECT_NAME" ]; then
+    echo "Error: Set the COMPOSE_PROJECT_NAME env variable."
+    return 1
+  fi
+
+  local -r other_containers=(
+    $(
+      docker ps --format '{{.Names}}' |
+        grep -v "^$COMPOSE_PROJECT_NAME"
+    )
+  )
+
+  docker stop "${other_containers[@]}"
+}
 
 alias d_tmp_ubuntu="docker run --rm -it ubuntu"
 
 # Start the Docker daemon if it is not running
 docker_start_daemon() {
   # Skip if Docker is already running
-  pgrep -x "Docker" >/dev/null && return
+  pgrep -x "Docker" > /dev/null && return
 
   echo -n "Starting the Docker daemon"
   open -a Docker
 
   # Wait for Docker to start
-  while ! docker ps &>/dev/null; do
+  while ! docker ps &> /dev/null; do
     sleep 0.3
     echo -n "."
   done
