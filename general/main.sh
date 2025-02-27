a184069154f2e636b8a8c8ce2c128411b6c2dad5
Peter Toth <tothpeter08@gmail.com>
2025-02-27 14:33:27 +0700

    Extract screen cast related code into its own file

--- general/main.sh
+++ general/main.sh
@@ -51,27 +51,6 @@ repeat_until_fails() {
   echo "FAILED: $@"
 }
 
-# ==============================================================================
-# Screen Cast Environment
-
-readonly _ORIGINAL_BACKGROUND_PATH=~/.dotfiles/local/original_background.txt
-readonly _NEW_BACKGROUND_PATH=/Volumes/DriveD/Dropbox/YT\ Brand/sizing_bg.jpg
-
-readonly _ORIGINAL_DOCK_SIZE_PATH=~/.dotfiles/local/original_dock_size.txt
-readonly _DESIRED_DOCK_SIZE=71
-
-screen_cast_env__activate() {
-  # Change the background
-  osascript -e 'tell application "System Events" to picture of every desktop' > $_ORIGINAL_BACKGROUND_PATH
-  osascript -e 'tell application "System Events" to set picture of every desktop to "'"$_NEW_BACKGROUND_PATH"'"'
-}
-
-screen_cast_env__deactivate() {
-  # Restore the background
-  original_background=$(cat $_ORIGINAL_BACKGROUND_PATH)
-  osascript -e 'tell application "System Events" to set picture of every desktop to "'"$original_background"'"'
-}
-
 is_url_open_in_browser() {
   local -r url=$1
 
