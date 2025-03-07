3fdab11d1afca5af451a3e8ac17c31cdea606ff4
Peter Toth <tothpeter08@gmail.com>
2025-03-07 13:32:58 +0700

    Update the screen cast env shortcuts

--- general/screen_cast.sh
+++ general/screen_cast.sh
@@ -2,18 +2,18 @@
 # Screen Cast Environment
 
 readonly _ORIGINAL_BACKGROUND_PATH=~/.dotfiles/local/original_background.txt
-readonly _NEW_BACKGROUND_PATH=/Volumes/DriveD/Dropbox/YT\ Brand/sizing_bg.jpg
+readonly _NEW_BACKGROUND_PATH=/Volumes/DriveD/Dropbox/Empire\ -\ collect/tech/desktop_bg/610_410.png
 
 readonly _ORIGINAL_DOCK_SIZE_PATH=~/.dotfiles/local/original_dock_size.txt
 readonly _DESIRED_DOCK_SIZE=71
 
-screen_cast_env::activate() {
+screen_cast_env__activate() {
   # Change the background
   osascript -e 'tell application "System Events" to picture of every desktop' > $_ORIGINAL_BACKGROUND_PATH
   osascript -e 'tell application "System Events" to set picture of every desktop to "'"$_NEW_BACKGROUND_PATH"'"'
 }
 
-screen_cast_env::deactivate() {
+screen_cast_env__deactivate() {
   # Restore the background
   original_background=$(cat $_ORIGINAL_BACKGROUND_PATH)
   osascript -e 'tell application "System Events" to set picture of every desktop to "'"$original_background"'"'
